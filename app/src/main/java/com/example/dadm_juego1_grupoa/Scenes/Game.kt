a783dd9a94dcf2ee94e42e5bdac783739d09c4aa
carlossantayana <carlossantayanavicente@gmail.com>
2024-10-16 18:07:57 +0200

    Game casi terminado

--- app/src/main/java/com/example/dadm_juego1_grupoa/Scenes/Game.kt
+++ app/src/main/java/com/example/dadm_juego1_grupoa/Scenes/Game.kt
@@ -1,6 +1,8 @@
 package com.example.dadm_juego1_grupoa.Scenes
 
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -9,10 +11,8 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.width
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.AccessTime
 import androidx.compose.material.icons.filled.Star
@@ -20,9 +20,9 @@ import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
@@ -37,9 +37,9 @@ import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import androidx.compose.ui.graphics.vector.ImageVector
-import androidx.navigation.NavController
 import com.example.dadm_juego1_grupoa.ui.theme.DADM_juego1_GrupoATheme
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 @Preview
 @Composable
@@ -55,19 +55,55 @@ fun BodyContentGame(/*navController: NavController*/){
     val brush = Brush.sweepGradient(colors, Offset.Zero)
 
     val numberOfQuestions: Int = 20
-    var questionsCompleted : Int by rememberSaveable{ mutableStateOf(0) }
+    var questionsCompleted : Int by rememberSaveable{ mutableStateOf(1) }
 
-    var points : Int by rememberSaveable { mutableStateOf(5000) }
-    var time : Int by rememberSaveable { mutableStateOf(32) }
+    var points : Int by rememberSaveable { mutableStateOf(0) }
+    var time : Int by rememberSaveable { mutableStateOf(30) }
+    var timePerQuestion : MutableList<Int> by remember { mutableStateOf(mutableListOf()) }
 
-    var question : String by rememberSaveable { mutableStateOf("Pregunta 1") }
+    val questionsAndAnwers = listOf(
+        "Pregunta 1" to listOf("Respuesta 1.1", "Respuesta 1.2", "Respuesta 1.3", "Respuesta correcta"),
+        "Pregunta 2" to listOf("Respuesta correcta", "Respuesta 2.2", "Respuesta 2.3", "Respuesta 2.4")
+    )
+
+    var currentQuestionIndex by rememberSaveable { mutableStateOf(0) }
+    var selectedAnswer by rememberSaveable { mutableStateOf<String?>(null) }
+
+    var answerColor by remember { mutableStateOf(Color.White) }
+    var areBottonsEnabled by rememberSaveable { mutableStateOf(true) }
+
+    LaunchedEffect(currentQuestionIndex){
+        while(time > 0){
+            delay(1000L)
+            if (selectedAnswer == null) {
+                time--
+            }
+        }
+        questionsCompleted++
+        if(currentQuestionIndex < questionsAndAnwers.lastIndex){
+            currentQuestionIndex++
+        }else{
+            currentQuestionIndex = 0
+        }
+        selectedAnswer = null
+        answerColor = Color.White
+        areBottonsEnabled = true
+        timePerQuestion.add(30)
+        time = 30
+    }
 
     Column(modifier = Modifier.fillMaxSize().background(brush), horizontalAlignment = Alignment.CenterHorizontally){
         Row(modifier = Modifier.fillMaxWidth().padding(top = 60.dp, bottom = 40.dp), horizontalArrangement = Arrangement.SpaceBetween){
-            Card(colors = CardDefaults.cardColors(containerColor = MaterialTheme.colorScheme.primary), modifier = Modifier.padding(start = 5.dp)){
-                Text("$questionsCompleted/$numberOfQuestions", style = TextStyle(fontSize = 40.sp), modifier = Modifier.padding(vertical = 8.dp, horizontal = 8.dp))
+            Card(colors = CardDefaults.cardColors(containerColor = MaterialTheme.colorScheme.primary), border = BorderStroke(5.dp, Color.Black), modifier = Modifier.padding(start = 5.dp)){
+                Box(modifier = Modifier.fillMaxWidth(0.4f).background(MaterialTheme.colorScheme.primary), contentAlignment = Alignment.Center) {
+                    Text(
+                        "$questionsCompleted/$numberOfQuestions",
+                        style = TextStyle(fontSize = 40.sp),
+                        modifier = Modifier.padding(vertical = 8.dp, horizontal = 8.dp)
+                    )
+                }
             }
-            Card(colors = CardDefaults.cardColors(containerColor = MaterialTheme.colorScheme.primary), modifier = Modifier.padding(end = 5.dp)){
+            Card(colors = CardDefaults.cardColors(containerColor = MaterialTheme.colorScheme.primary), border = BorderStroke(5.dp, Color.Black), modifier = Modifier.padding(end = 5.dp)){
                 Box(modifier = Modifier.fillMaxWidth(0.7f).background(MaterialTheme.colorScheme.primary)) {
                     Row(
                         modifier = Modifier.align(Alignment.CenterEnd),
@@ -89,11 +125,42 @@ fun BodyContentGame(/*navController: NavController*/){
             }
         }
 
-        QuestionAnswerCards(modifier = Modifier.weight(1f))
+        QACard(questionsAndAnwers[currentQuestionIndex].first, onClick = {}, modifier = Modifier.weight(1f), color = MaterialTheme.colorScheme.primary)
+
+        var answers = questionsAndAnwers[currentQuestionIndex].second
+        answers.forEach{
+            answer ->
+            QACard(answer, isAnswer = true, onClick = {
+                if (areBottonsEnabled){
+                    selectedAnswer = answer
+                    answerColor = if (answer == "Respuesta correcta") Color.Green else Color.Red
+                    areBottonsEnabled = false
+                    timePerQuestion.add(30 - time)
+
+                    kotlinx.coroutines.GlobalScope.launch{
+                        delay(2000L)
+                        questionsCompleted++
+                        if (answer == "Respuesta correcta"){
+                            points += 100
+                        }
+
+                        if (currentQuestionIndex < questionsAndAnwers.lastIndex){
+                            currentQuestionIndex++
+                        }else{
+                            currentQuestionIndex = 0
+                        }
+                        selectedAnswer = null
+                        answerColor = Color.White
+                        areBottonsEnabled = true
+                        time = 30
+                    }
+                }
+            }, modifier = Modifier.weight(1f), color = if (selectedAnswer == answer) answerColor else Color.White)
+        }
 
         Spacer(modifier = Modifier.weight(1f))
 
-        Card(colors = CardDefaults.cardColors(containerColor = MaterialTheme.colorScheme.primary), modifier = Modifier.padding(bottom = 5.dp).align(Alignment.CenterHorizontally)){
+        Card(colors = CardDefaults.cardColors(containerColor = MaterialTheme.colorScheme.primary), border = BorderStroke(5.dp, Color.Black), modifier = Modifier.padding(bottom = 5.dp).align(Alignment.CenterHorizontally)){
             Box(modifier = Modifier.fillMaxWidth(0.5f).background(MaterialTheme.colorScheme.primary), contentAlignment = Alignment.Center) {
                 Row(verticalAlignment = Alignment.CenterVertically) {
                     Icon(
@@ -116,21 +183,13 @@ fun BodyContentGame(/*navController: NavController*/){
 }
 
 @Composable
-fun QuestionAnswerCards(modifier : Modifier = Modifier, question : String = "Pregunta 1", answers : List<String> = listOf("Respuesta 1", "Respuesta 2", "Respuesta 3", "Respuesta 4")){
-    QACard(question, modifier)
-    for (answer in answers){
-        QACard(answer, modifier)
-    }
-}
-
-@Composable
-fun QACard(qa : String, modifier : Modifier = Modifier){
-    Card(colors = CardDefaults.cardColors(containerColor = MaterialTheme.colorScheme.primary), modifier = modifier.padding(vertical = 8.dp, horizontal = 4.dp)){
-        Box(modifier = Modifier.fillMaxWidth(0.9f).fillMaxHeight().background(MaterialTheme.colorScheme.primary), contentAlignment = Alignment.Center) {
+fun QACard(qa : String, modifier : Modifier = Modifier, isAnswer : Boolean = false, onClick: () -> Unit, color: Color){
+    Card(colors = CardDefaults.cardColors(color), border = BorderStroke(5.dp, Color.Black), modifier = modifier.padding(vertical = 8.dp, horizontal = 4.dp)){
+        Box(modifier = Modifier.fillMaxWidth(0.9f).fillMaxHeight().clickable(enabled = onClick != {}, onClick = onClick), contentAlignment = Alignment.Center) {
             Text(
                 "$qa",
                 modifier = Modifier.padding(vertical = 8.dp, horizontal = 8.dp),
-                style = TextStyle(fontSize = 40.sp)
+                style = TextStyle(fontSize = if (isAnswer) 20.sp else 40.sp)
             )
         }
     }
